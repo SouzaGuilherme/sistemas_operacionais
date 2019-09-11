6b113cb6387326fc1cb83a3363686133b79fd17a
Guilherme de Souza da Silva <gdsdsilva@inf.ufpel.edu.br>
2019-09-11 17:41:40 -0300

    loop in finish consumer

--- code_producer_consumer.cpp
+++ code_producer_consumer.cpp
@@ -22,17 +22,61 @@ using namespace std;
 list <int> buffer;
 pthread_mutex_t lock;
 
+bool number_cousin(int number);
+
 void *produce(void *v){
         parameter_produce *acess = (parameter_produce*)v;
-        int iterations_producer;
+        int iterations_producer = 0;
 
+        srand(time(nullptr));							/* Seed for rand() based in hour */				
         while(iterations_producer < acess->iteration_passed){
-          if(buffer.size() == acess->max_size_list)
-                  cout << "wait" << endl;
+                /* Posso usar um enquanto */
+                if(buffer.size() == acess->max_size_list)
+                        cout << "wait" << endl;
+
+                pthread_mutex_lock(&lock);
+                /* Start section critic */
+                buffer.push_back(rand());
+                /* Finish section critic */
+                pthread_mutex_unlock(&lock);
+
+                iterations_producer++;
         }
 }
 
 void *consumer(void *v){
+        while(true){
+                cout << "vazia? " << buffer.empty() << endl;
+               /* while(buffer.empty() == true){
+                        cout << "preso vazioooooo" << endl;
+                        usleep(1); 
+                }*/
+                if(buffer.empty() == true){
+                        while(true){
+                                if(buffer.empty() == false){
+                                        cout << "saindo do vazio" << endl;
+                                        break;
+                                }
+                        }
+                }
+
+                pthread_mutex_lock(&lock);
+
+                cout << "Dentro? " << buffer.front() << endl;
+                bool check = false;
+                if(buffer.front() == -1){
+                        cout << "TIRA: --> " << buffer.front() << endl;
+                        break;
+                }
+                check = number_cousin(buffer.front());
+                buffer.pop_front();
+                pthread_t id_son = pthread_self();
+                cout << "Thread ID: " << id_son << endl;                
+
+                pthread_mutex_unlock(&lock);
+                cout << "TO AQUI" << endl;
+        }
+        cout << "indo embora" << endl;
 }
 
 int main(int argc, char *argv[]){
@@ -50,9 +94,18 @@ int main(int argc, char *argv[]){
 
         /* Get ID of thread main */
         pthread_t main_id = pthread_self();
-        cout << main_id << endl;
+        cout << "main id: " << main_id << endl;
 
-        pthread_t thread_produce[number_producer];
+        pthread_t *thread_produce;
+        pthread_t *thread_consumer;
+
+        if(((thread_produce = (pthread_t*)malloc(number_producer*sizeof(pthread_t))) == NULL)){
+                DIE("Erro'r alloc producers")
+        }
+
+        if((thread_consumer = (pthread_t*)malloc(number_consumer*sizeof(pthread_t))) == NULL){
+                DIE("Erro'r alloc consumers")
+        }
 
         /* Created of producers */
         for(int i = 0; i < number_producer; ++i){
@@ -61,9 +114,64 @@ int main(int argc, char *argv[]){
         }
 
 
-        /* Join threads od producers */
+
+
+        /* ZONA DE TESTE */
+/*
+        cout << "Finish1" << endl;
+
+        list <int> :: iterator i;
+        for(i = buffer.begin(); i != buffer.end(); i++)
+                cout << *i << endl;
+*/
+        /* ------------------------------------- */
+
+        /* Created of consumers */
+        for(int i = 0; i < number_consumer; ++i){
+                if(pthread_create(&thread_consumer[i], NULL, consumer, (void*)&p_acess))
+                        DIE("Not created threads producers");
+        }
+
+
+        /* Join threads of producers */
         for(int i = 0; i < number_producer; ++i){
                 if(pthread_join(thread_produce[i], NULL))
                         DIE("Not join threads");
         }
+        cout << "Passou producer" << endl;
+
+        for(int j = 0; j < number_consumer; j++)
+                buffer.push_back(-1);
+
+        for(auto v : buffer)
+                cout << v << endl;
+        /* Join threads of consumers */
+        for(int r = 0; r < number_consumer; r++){
+                cout << "preso join consumer "<< r << endl;
+                if(pthread_join(thread_consumer[r], NULL))
+                        DIE("Not join threads consumers");
+        }
+
+        cout << "Finish join" << endl;
+        //free(thread_produce);
+        //free(thread_consumer);
+        cout << "Finish all" << endl;
 }
+
+/* Function verifyc if the number passed is cousin */
+bool number_cousin(int number){
+	bool is_cousin = true;						/* I assume it is cousin */
+	int divider = 2;						/* First Divider */
+	
+	if(number <= 1)							/* Check if it's positive */
+		return false;
+
+	while((is_cousin == true) && (divider <= number/2)){		/* Division secessive */
+		int rest = number % divider;
+		if(rest == 0)
+			is_cousin = false;
+		divider++;	
+	}
+
+	return is_cousin;
+};
